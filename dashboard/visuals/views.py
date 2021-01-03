92c030a8a0d52961b084add99a2960f14d3bad6e
marco <cagliamarco@gmail.com>
2021-01-03 10:45:21 +0100

    Moved Media Creation to views.py

--- dashboard/visuals/views.py
+++ dashboard/visuals/views.py
@@ -1,14 +1,53 @@
 import os
 from pathlib import Path
 
+from content_analysis.media_creation.media_creator import MediaCreator
+from content_analysis.media_creation.preprocessing import Preprocessor
+from content_analysis.media_creation.exceptions import NoDataError
 from django.shortcuts import render
 from dotenv import load_dotenv
+import pandas as pd
+from bokeh.embed import components
+from sqlalchemy import create_engine
 
 load_dotenv()
 
-DATA_PATH = Path(__file__).parent.parent.joinpath(os.getenv("DATA_PATH"))
+DATA_PATH = Path(__file__).parent.parent.joinpath(os.getenv("DATA_PATH")) \
+    .joinpath(os.getenv("DATABASE_NAME"))
 
 # Create your views here.
+
+
+def _create_media(kind):
+    # Check if the DB is at the expected position
+    if not DATA_PATH.isfile():
+        raise NoDataError(f"No DB at {DATA_PATH}")
+
+    # Create DB for loading the data
+    engine = create_engine(f"sqlite:///{DATA_PATH.as_posix}")
+
+    # Load the data with pandas
+    data = pd.read_sql("SELECT * FROM video_info", engine)
+
+    # Preprocess the data
+    preprocessed = Preprocessor(data).preprocess
+
+    # Initialize Media Creator
+    media_creator = MediaCreator(preprocessed)
+
+    # Create and return the appropriate graphs
+    if kind == "overview":
+        graphs = media_creator.generate_overviews()
+    elif kind == "ot_graphs":
+        graphs = media_creator.generate_ot_plots()
+    else:
+        raise AssertionError
+
+    # Return components as list of tuples
+    graph_components = [components(graph) for graph in graphs]
+
+    return graph_components
+
 
 
 def home(request):
@@ -17,17 +56,8 @@ def home(request):
 
 
 def overviews(request):
-    # Navigate to folder where overview graphs are stored
-    overviews_folder = DATA_PATH.joinpath("overviews")
-
-    # Generate graphs paths
-    graphs_paths = overviews_folder.iterdir()
-
-    # Load all graphs
-    graph_htmls = []
-    for path in graphs_paths:
-        with path.open("r") as f:
-            graph_htmls.append(f.read())
+    # Create Media
+    media = _create_media("overviews")
 
     return render(
         request,
