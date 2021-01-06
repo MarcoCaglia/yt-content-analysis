1118af3f3c2e84439fe3121019f915b4b2610f2b
marco <cagliamarco@gmail.com>
2021-01-06 07:39:56 +0100

    Feature: Pipeline uploads now second table; comments

--- content_sourcing/asmr_scraper/pipelines.py
+++ content_sourcing/asmr_scraper/pipelines.py
@@ -18,6 +18,16 @@ class AsmrScraperPipeline:
 
 
 class SqlPipeline:
+
+    VIDEO_INFO_FILEDS = [
+        "title", "upload_date", "views", "author", "likes", "dislikes",
+        "comments", "timestamp", "video_url", "video_id"
+        ]
+    COMMENTS_FIELDS = ["video_id", "comments"]
+    VIDEO_TABLE_NAME = "video_info"
+    COMMENTS_TABLE_NAME = "comments"
+
+
     def __init__(self, data_path) -> None:
         self.data_path = os.path.abspath(data_path)
 
@@ -33,12 +43,37 @@ class SqlPipeline:
         self.con = create_engine("sqlite:///" + self.data_path)
 
     def process_item(self, item, spider):
-        item = dict(item)
-        item = {key: [item[key]] for key in item}
-        df = pd.DataFrame.from_dict(dict(item))
-        df.to_sql(
-            "video_info",
-            con=self.con,
-            index=False,
-            if_exists="append"
-        )
+        # Split item into video information and comments
+        video_info, comments = self._split_item(item)
+
+        # Construct list of tuples with (table_names table)
+        upload_data = [
+            (SqlPipeline.VIDEO_TABLE_NAME, video_info),
+            (SqlPipeline.COMMENTS_TABLE_NAME, comments)
+        ]
+
+        # Iterate over constructed list and upload second element of tuple
+        # (table) and name the uploaded table with the first element of the
+        # table (name).
+        for name, table in upload_data:
+            table.to_sql(
+                name,
+                con=self.con,
+                index=False,
+                if_exists="append"
+            )
+
+    def _split_item(self, item):
+        # Split items in video info fields and comments fields.
+        # Bot tables can be connected via video_id.
+        video_info = {
+            key: [item[key]] for key in SqlPipeline.VIDEO_INFO_FILEDS
+            }
+        comments = {key: item[key] for key in SqlPipeline.COMMENTS_FIELDS}
+
+        # Convert both dictionaries to dataframes
+        video_info = pd.DataFrame.from_dict(dict(video_info))
+        comments = pd.DataFrame.from_dict(dict(comments))
+
+        # Return both tables
+        return video_info, comments
